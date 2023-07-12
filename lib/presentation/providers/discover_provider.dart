6275d511157ed3452b46d645a0091358e1d857f7
Dennys Reyes <dennysgraph@gmail.com>
2023-07-12 16:15:55 -0400

    commit: Toktik app

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+  //TODO: repository, dataSource
+
   bool initialLoading = true;
   List<VideoPost> video = [];
 
