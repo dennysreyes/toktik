18099766a8f4feb1156859ef8c93b764e9de26c6
Dennys Reyes <dennysgraph@gmail.com>
2023-07-12 15:08:05 -0400

    TokTik app

---
+++ lib/config/theme/theme_data.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ThemeApp {
+  ThemeData appTheme() => ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.dark);
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:toktik/config/theme/theme_data.dart';
+import 'package:toktik/presentation/providers/discover_provider.dart';
+import 'package:toktik/presentation/screen/discover/discover_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+            create: (_) => DiscoverProvider())
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeApp().appTheme(),
+          title: 'Material App',
+          home: const DiscoverScreen()),
+    );
+  }
+}
